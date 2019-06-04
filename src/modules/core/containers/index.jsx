d6655923e2157c71995e9f428edf678b8359425c
Whitney Burton <whitneyburton9@gmail.com>
2019-06-04 14:39:16 -0600

    WIP: Render timestamp on each message and begin controlled form functionality

--- src/modules/core/containers/index.jsx
+++ src/modules/core/containers/index.jsx
@@ -1,49 +1,79 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-import {selectors, addMessage} from "../redux/home";
+import { selectors, addMessage } from '../redux/home';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
-      return (
-          <div className={"row"}>
-              <div className={"column"}>
-                  <h2>Input</h2>
-                  {this.renderInput()}
-              </div>
-              <div className={"column"}>
-                  <h2>Messages</h2>
-                  {this.renderMessages()}
-              </div>
-          </div>
-      );
+    return (
+      <div className={'row'}>
+        <div className={'column'}>
+          <h2>Input</h2>
+          {this.renderInput()}
+        </div>
+        <div className={'column'}>
+          <h2>Messages</h2>
+          {this.renderMessages()}
+        </div>
+      </div>
+    );
   }
 
   /// Renders the list of messages wrapped in paragraph tags
   renderMessages() {
-      var count = 0;
-      const messages = this.props.messages.map((message) =>
-        <p key={count++}>{message}</p>
+    var count = 0;
+    const messages = this.props.messages.map(message => {
+      return (
+        <Fragment>
+          <p key={count++}>{message.message}</p>
+          <p key={count++}>{message.timestamp}</p>
+        </Fragment>
       );
+    });
 
-      return messages;
+    return messages;
+  }
+
+  handleUserName(e) {
+
   }
 
   /// Renders a single input for the user to enter messages
   renderInput() {
-      return (
-          <div>
-              <label>Message:
-                <input id="message-input" type="text" />
-              </label>
-              <input type="submit" value="Submit" onClick={() => {
-                  this.props.addMessage(document.getElementById("message-input").value);
-                  document.getElementById("message-input").value = '';
-              }} />
-          </div>
-      );
+    return (
+      <div>
+        <label>
+          User Name:
+          <input
+            id='username-input'
+            type='text'
+            onChange={() => this.handleUserName()}
+          />
+        </label>
+        <label>
+          Message:
+          <input id='message-input' type='text' />
+        </label>
+        <input
+          type='submit'
+          value='Submit'
+          onClick={() => {
+            this.props.addMessage(
+              document.getElementById('message-input').value
+            );
+            document.getElementById('message-input').value = '';
+          }}
+        />
+      </div>
+    );
   }
 }
 
@@ -55,11 +85,13 @@ const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    messages: selectors.getMessages(state),
+    messages: selectors.getMessages(state)
   };
 };
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
